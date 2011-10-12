7e2cdd8eeba45d9b81b6322819a23ff58171dbba
Christian Bäuerlein <fabrik42@gmail.com>
2011-10-12 21:48:40 +0200

    finishes text asset helper specs

--- lib/text.rb
+++ lib/text.rb
@@ -28,7 +28,7 @@ module Text
     text = '' if text.nil?
     text = text.dup.to_str
     text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
-    text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> brt
+    text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
     text
   end
 
